23ea9faeabf8d5edf4b95acb67a03f335f807406
caoxingyu <caoxingyu2016@qq.com>
2018-11-17 15:56:57 +0800

    淡淡的

---
+++ src/main/java/com/example/takeTicket/vo/GetPointRecordVo.java
@@ -0,0 +1,66 @@
+package com.example.takeTicket.vo;
+
+public class GetPointRecordVo {
+	 private String custId;
+	 
+	 private String shopId;
+	 
+	 private String childId;
+	 
+	 private String recordFlg;
+	 
+	 private String createTime;
+	 
+	 private String bakStr;
+
+	public String getCustId() {
+		return custId;
+	}
+
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+
+	public String getShopId() {
+		return shopId;
+	}
+
+	public void setShopId(String shopId) {
+		this.shopId = shopId;
+	}
+
+	public String getChildId() {
+		return childId;
+	}
+
+	public void setChildId(String childId) {
+		this.childId = childId;
+	}
+
+	public String getRecordFlg() {
+		return recordFlg;
+	}
+
+	public void setRecordFlg(String recordFlg) {
+		this.recordFlg = recordFlg;
+	}
+
+	public String getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getBakStr() {
+		return bakStr;
+	}
+
+	public void setBakStr(String bakStr) {
+		this.bakStr = bakStr;
+	}
+	 
+	 
+
+}
