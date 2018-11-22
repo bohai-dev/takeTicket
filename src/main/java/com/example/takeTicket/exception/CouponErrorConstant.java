727c76602f394de1811b6b35e2a8b9d1d6b0e2e3
caoxingyu <caoxingyu2016@qq.com>
2018-11-22 15:43:21 +0800

    Merge branch 'master' of https://github.com/bohai-dev/takeTicket
    # Conflicts:
    #	src/main/java/com/example/takeTicket/exception/CouponErrorConstant.java

--- src/main/java/com/example/takeTicket/exception/CouponErrorConstant.java
+++ src/main/java/com/example/takeTicket/exception/CouponErrorConstant.java
@@ -10,8 +10,6 @@ public enum CouponErrorConstant {
     SUCCESS("0000", "ok"),
 
     LOGIN_WEIXIN_ERROR("0001","微信登录异常"),
-
-
     ADMIN_LOGIN_ERROR("0002","用户名或密码错误"),
     COUPON_NOT_EXISTS_ERROR("0003","该优惠券不存在"),
     COUPON_USED_ERROR("0004","该优惠券已被使用"),
