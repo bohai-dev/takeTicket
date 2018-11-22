15c0e1d47e081a35d54842750fa882b4ea342232
caoxingyu <caoxingyu2016@qq.com>
2018-11-22 11:26:45 +0800

    错误码

--- src/main/java/com/example/takeTicket/exception/CouponErrorConstant.java
+++ src/main/java/com/example/takeTicket/exception/CouponErrorConstant.java
@@ -10,12 +10,12 @@ public enum CouponErrorConstant {
     SUCCESS("0000", "ok"),
 
     LOGIN_WEIXIN_ERROR("0001","微信登录异常"),
-    QRCODE_PNG_ERROR("0001","二维码生成PNG图片错误"),
+
 
     ADMIN_LOGIN_ERROR("0002","用户名或密码错误"),
     COUPON_NOT_EXISTS_ERROR("0003","该优惠券不存在"),
     COUPON_USED_ERROR("0004","该优惠券已被使用"),
-
+    QRCODE_PNG_ERROR("0005","二维码生成PNG图片错误"),
 
 
     UNKNOW_EXCEPTION("1000", "程序内部异常" );
